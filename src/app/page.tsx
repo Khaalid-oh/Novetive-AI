a528448d1d10594a0bdbb65871edd616be31c473
Khalid Sulyman <84248666+Khaliid-oh@users.noreply.github.com>
2024-05-07 11:14:56 +0100

    Novetive 3.5

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="h-screen w-full mx-auto text-3xl font-bold to-blue-600">
+      Novetive AI 3.5
+    </div>
+  );
+}
+
+export default page;
